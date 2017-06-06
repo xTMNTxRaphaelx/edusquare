7a4d8972db2e8f9d07ef0b9fc9a0d493a8a53895
Rahul Garg <rahul_garg08@yahoo.com>
2017-06-06 18:33:00 +0530

    did what i could for now

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
     'use strict';
-     $('.results .carousel.carousel-slider').carousel({fullWidth: true});
+    $('.results .carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+
+    $('.past-acheivers ul.tabs').tabs('select_tab', 'acheivers__medical');
+    $('.past-acheivers .carousel').carousel({
+        // dist: 0,
+        // padding: 60,
+        // noWrap: true,
+        // indicators: true,
+        // shift: 99
+    });
+
+    $('.peoples.carousel').carousel({
+        duration: 450,
+        dist: -70
+    });
+
+    $('.peoples.carousel a').click(function (e) {
+        e.preventDefault();
+        var currentVal = $(this).find('img').data('testimonial');
+        $('.peoples-testimonials .testimonial').addClass('hide');
+        $('.peoples-testimonials .testimonial#' + currentVal).removeClass('hide');
+    });
 });
--- js/testimonials.js
+++ js/testimonials.js
@@ -1,15 +1,4 @@
 'use strict';
 $('document').ready(function (e) {
 
-    $('.peoples.carousel').carousel({
-        duration: 450,
-        dist: -70
-    });
-    
-    $('.peoples.carousel a').click(function (e) {
-        e.preventDefault();
-        var currentVal = $(this).find('img').data('testimonial');
-        $('.peoples-testimonials .testimonial').addClass('hide');
-        $('.peoples-testimonials .testimonial#'+currentVal).removeClass('hide');
-    });
 }); 
