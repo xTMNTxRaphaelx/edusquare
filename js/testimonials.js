1e50d54a898baaecb74a3203388abd6fcdbbf061
Rahul Garg <rahul_garg08@yahoo.com>
2017-06-06 17:56:56 +0530

    people testimonial repair

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,13 +1,15 @@
 'use strict';
 $('document').ready(function (e) {
 
-    $('.peoples.carousel').carousel();
-    $('.testimonial-carousel.carousel').carousel();
+    $('.peoples.carousel').carousel({
+        duration: 450,
+        dist: -70
+    });
     
     $('.peoples.carousel a').click(function (e) {
         e.preventDefault();
         var currentVal = $(this).find('img').data('testimonial');
-        $('.testimonials-section .testimonial').addClass('hide');
-        $('.testimonials-section .testimonial#'+currentVal).removeClass('hide');
+        $('.peoples-testimonials .testimonial').addClass('hide');
+        $('.peoples-testimonials .testimonial#'+currentVal).removeClass('hide');
     });
-});
+}); 
