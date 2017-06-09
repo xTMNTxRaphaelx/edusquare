f33a0a819a3ae1efecb6e7c1c8016d64098f1594
Rahul Garg <rahul_garg08@yahoo.com>
2017-06-09 16:36:36 +0530

    more pages created

--- js/main.js
+++ js/main.js
@@ -59,15 +59,15 @@ $("document").ready(function() {
   //   $(".past-acheivers ul.tabs").tabs("select_tab", "acheivers__medical");
   //   $(".past-acheivers .carousel").carousel({});
 
-  //   $(".peoples.carousel").carousel({
-  //     duration: 450,
-  //     dist: -70
-  //   });
+  $(".peoples.carousel").carousel({
+    duration: 450,
+    dist: -70
+  });
 
-  //   $(".peoples.carousel a").click(function(e) {
-  //     e.preventDefault();
-  //     var currentVal = $(this).find("img").data("testimonial");
-  //     $(".peoples-testimonials .testimonial").addClass("hide");
-  //     $(".peoples-testimonials .testimonial#" + currentVal).removeClass("hide");
-  //   });
+  $(".peoples.carousel a").click(function(e) {
+    e.preventDefault();
+    var currentVal = $(this).find("img").data("testimonial");
+    $(".peoples-testimonials .testimonial").addClass("hide");
+    $(".peoples-testimonials .testimonial#" + currentVal).removeClass("hide");
+  });
 });
