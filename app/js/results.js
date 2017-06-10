793d27a2f671da5d53cd2795c5be9922c865e47a
n3m0 <rahul_garg08@yahoo.com>
2017-06-10 19:19:58 +0530

    foundation contetn added

--- app/js/results.js
+++ app/js/results.js
@@ -1,40 +1,59 @@
 $("document").ready(function() {
   $(".scrollspy").scrollSpy();
-  // $('.results-item').pushpin({
-  //     top: 100,
-  //     bottom: 0
-  // });
+  
   var trString = "";
 
-  function loadTable(tabledata, id) {
+  function loadTable(tabledata, id, limit) {
+    var isViewMore= false;
     $.each(tabledata, function(i, data) {
       if (i === 0) {
         return;
       }
       var currentData = data;
-      trString = "<tr>";
+      if(limit && i > limit) {
+        trString = "<tr class='hide extra'>";
+        isViewMore= true;
+      } else {
+        trString = "<tr>";
+      }
+      
       for (var j = 0; j < currentData.length; j++) {
         trString += "<td>" + currentData[j] + "</td>";
       }
       trString += "</tr>";
-      console.log(id);
       $(id).find("tbody").append(trString);
+
     });
+    if(isViewMore) {
+      $(id).after('<br /><a class="btn viewMoreBtn notExpanded right">View More</a>');
+      $('.viewMoreBtn').click(function(e) {
+        if($(this).hasClass('notExpanded')) {
+          $(id).find('tbody tr.hide').removeClass('hide');
+          $(this).removeClass('notExpanded').addClass('expanded').text('View Less');
+        } else {
+          $(id).find('tbody tr.extra').addClass('hide');
+          $(this).removeClass('expanded').addClass('notExpanded').text('View More');
+          $('html,body').animate({
+            scrollTop: $(this).offset().top-100},
+            'slow');
+        }
+      });
+    }
   }
 
-  function loadResults(csvUrl, id) {
+  function loadResults(csvUrl, id, limit) {
     $.ajax({
       url: "assets/results/" + csvUrl,
       success: function(csvFile) {
         var csvData = $.csv.toArrays(csvFile);
-        loadTable(csvData, id);
+        loadTable(csvData, id, limit);
       }
     });
   }
-  loadResults("2017engg.csv", "#Results__JeeMain2017");
+  loadResults("2017engg.csv", "#Results__JeeMain2017", 23);
   loadResults("2016NEET.csv", "#Results__NEET2016");
   loadResults("2016PMET.csv", "#Results__PMET2016");
-  loadResults("2016JEEADV.csv", "#Results__JeeAdv2016");
+  loadResults("2016JEEADV.csv", "#Results__JeeAdv2016", 23);
   loadResults("2015Results.csv", "#Results__MedicalJEE2015");
 
   function stickyNess() {
